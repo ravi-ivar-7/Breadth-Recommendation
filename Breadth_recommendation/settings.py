44e9adafde678ed9a0c1eb96105f159869973cee
ravi-ivar-7 <ravikumar117211@gmail.com>
2024-03-02 11:22:37 +0530

    changed db

--- Breadth_recommendation/settings.py
+++ Breadth_recommendation/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = "Breadth_recommendation.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': '55fGA6eACGe534*fc51C15gBEgaf2ffe',
+        'HOST': 'roundhouse.proxy.rlwy.net',
+        'PORT': '21338',
     }
 }
 
